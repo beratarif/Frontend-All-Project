8a6fd57de63900eae9a72620dd4f0ef7533c01ac
Berat Arif Gönül <163324501+beratarif@users.noreply.github.com>
2026-02-10 16:29:36 +0300

    Delete beratgnl.online.v2/assets directory

--- beratgnl.online.v2/assets/js/contact.js
+++
@@ -1,6 +0,0 @@
-const contactForm = document.getElementById("contact-form");
-    contactForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-      alert("Mesajınız için teşekkürler! En kısa sürede dönüş yapacağım. 😊");
-      contactForm.reset();
-    });
--- beratgnl.online.v2/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-import "./project.js";
-import "./theme.js";
-import "./typing.js";
-import "./scroll.js";
-import "./contact.js";
