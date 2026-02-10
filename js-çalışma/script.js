770cc605e1de1419b372387d64177baeb214738e
Berat Arif Gönül <gonulberat1@gmail.com>
2026-02-10 14:28:39 +0300

    Js Gelişimi

---
+++ js-çalışma/script.js
@@ -0,0 +1,9 @@
+document.getElementById('background').style.background = "#000000";
+
+document.getElementById('background').style.color = "#fff";
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener("click", () => {
+  const randomColor = "#" + math.random
+})
